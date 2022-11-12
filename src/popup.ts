7269f1a6a44b54a21ff006aff7d8e909892ba5b7
Cside <cside.story@gmail.com>
2022-11-12 17:56:42 +0900

    とりあえず必要な情報は全部引っ張ってきた

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,6 @@
+import debounce from 'lodash.debounce';
+import escape from 'lodash.escape';
+
 const NOTION_HOST = 'https://www.notion.so';
 const NOTION_SEARCH_URL = `${NOTION_HOST}/api/v3/search`;
 const DEBOUNCE_TIME = 150;
@@ -14,18 +17,19 @@ function idToUuid(path: string) {
 type Res = {
   results: {
     id: string;
-    highlight: {
-      text: string;
-      title?: string;
-    };
+    highlight: { text: string; title?: string };
     highlightBlockId: string;
+    featureGroups?: object[]; // TODO: 最後に消す
+    sources?: string[]; // TODO: 最後に消す
+    analytics?: object;
   }[];
   recordMap: {
     block: {
       [id: string]: {
         value: {
           properties: { title: string[][] };
-          parent_id: string;
+          parent_id?: string;
+          format?: { page_icon?: string };
         };
       };
     };
@@ -70,46 +74,91 @@ const search = async (query: string) => {
     },
   });
   const res = (await response.json()) as Res;
-  const result: any = res.results.map((result) => {
+  const results: any = res.results.map((result) => {
+    const recordMap = res.recordMap;
+    const value = recordMap.block[result.id].value;
+
+    const parentPaths: string[] = [];
+    const getParentPath = (parentId: string) => {
+      if (!recordMap.block[parentId]) return;
+
+      const value = recordMap.block[parentId].value;
+      parentPaths.push(
+        value.properties.title.map((array) => array[0]).join(''),
+      );
+      if (value.parent_id) getParentPath(value.parent_id);
+    };
+    if (value.parent_id) getParentPath(value.parent_id);
+
+    const pageIcon = value.format?.page_icon;
+
     if (result.highlight.title) {
       return {
         title: result.highlight.title,
         url: NOTION_HOST + '/' + result.id.replaceAll('-', ''),
+        pageIcon,
+        parentsPath: parentPaths.reverse().join(' / '), // TODO: 存在しないキーではキーごとなくしたい
       };
     } else {
       return {
-        title: res.recordMap.block[result.id].value.properties.title
-          .map((array) => array[0])
-          .join(''),
+        title: value.properties.title.map((array) => array[0]).join(''),
         text: result.highlight.text,
-        url:
-          NOTION_HOST +
-          '/' +
-          result.id.replaceAll('-', '') +
-          '#' +
-          result.highlightBlockId.replaceAll('-', ''),
+        url: `${NOTION_HOST}/${result.id.replaceAll(
+          '-',
+          '',
+        )}#${result.highlightBlockId.replaceAll('-', '')}`,
+        pageIcon,
+        parentsPath: parentPaths.reverse().join(' / '),
       };
     }
   });
-  resultElem.innerText = JSON.stringify(result, null, 2);
+
+  const svg = `
+    <svg viewBox="0 0 30 30" style="width: 18px; height: 18px; display: block; fill: rgba(55, 53, 47, 0.45); flex-shrink: 0; backface-visibility: hidden;">
+      <g>
+        <path d="M16,1H4v28h22V11L16,1z M16,3.828L23.172,11H16V3.828z M24,27H6V3h8v10h10V27z M8,17h14v-2H8V17z M8,21h14v-2H8V21z M8,25h14v-2H8V25z">
+        </path>
+      </g>
+    </svg>`;
+
+  resultElem.innerHTML = results
+    .map((result: any) => {
+      result.title = escape(result.title).replace(
+        /&lt;gzkNfoUU&gt;(.+?)&lt;\/gzkNfoUU&gt;/g,
+        '<b>$1</b>',
+      );
+      if (result.text)
+        result.text = escape(result.text).replace(
+          /&lt;gzkNfoUU&gt;(.+?)&lt;\/gzkNfoUU&gt;/g,
+          '<b>$1</b>',
+        );
+      result.parentsPath = escape(result.parentsPath);
+
+      return `
+          <div style="border: solid 3px black;">
+              <p>
+                ${result.pageIcon || svg}
+                ${result.title}
+              </p>
+              ${result.parentsPath ? `<p>${result.parentsPath}</p>` : ''}
+              ${result.text ? `<p>${result.text}</p>` : ''}
+          </div>
+  `;
+    })
+    .join('\n');
+
   const resElem = document.getElementById('res');
+  res.results.forEach((r) => {
+    delete r.featureGroups;
+    delete r.sources;
+    delete r.analytics;
+  });
   if (resElem) resElem.innerText = JSON.stringify(res, null, 2);
 };
 
-const debounce = (fn: (...args: any[]) => void, wait: number) => {
-  let timerId: number | null = null;
-  return (...args: any[]) => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-    timerId = window.setTimeout(() => {
-      fn(...args);
-    }, wait);
-  };
-};
 /* TODO
-  - favicon
-  - parents name
+  - 起動時は last modified でソートしても良い気がするが ... query="" の検索無理じゃね？
+  - XSS ...
 */
 
 const onInput = debounce(search, DEBOUNCE_TIME);
