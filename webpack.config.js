28858e2d948aca591955b5090d5cf2dbc72fe1ac
Cside <cside.story@gmail.com>
2022-12-03 11:05:01 +0900

    use less syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,97 @@
 const merge = require('lodash.merge');
 const TerserPlugin = require('terser-webpack-plugin');
-
-const jsConfig = (() => {
-  let config = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: {
-      background: './src/ts/background.ts',
-      popup: './src/ts/popup.tsx',
-      options: './src/ts/options.ts',
-    },
-    output: {
-      path: `${__dirname}/public/js`,
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-env',
-                    {
-                      targets: ['>= 0.2% and Chrome >= 1'],
-                      useBuiltIns: 'usage',
-                      corejs: 3,
-                    },
-                  ],
-                ],
-              },
-            },
-            'ts-loader',
-          ],
-        },
-        {
-          test: /\.pcss$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    [
-                      'postcss-preset-env',
-                      {
-                        features: {
-                          'nesting-rules': true,
-                        },
-                      },
-                    ],
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      ],
-    },
-    optimization: {
-      splitChunks: {
-        name: 'vendor',
-        chunks: (chunk) => chunk.name !== 'background',
-      },
-    },
-    watchOptions: {
-      ignored: /node_modules/,
-    },
-    experiments: { topLevelAwait: true },
-  };
-
-  if (config.mode === 'development') {
-    config = merge(config, {
-      devtool: 'inline-source-map',
-    });
-  } else {
-    config = merge(config, {
-      plugins: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              pure_funcs: ['console.log', 'console.info'],
-            },
-          },
-        }),
-      ],
-    });
-  }
-  return config;
-})();
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 /** @type import('webpack').Configuration */
-module.exports = jsConfig;
+let config = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    background: './src/ts/background.ts',
+    popup: './src/ts/popup.tsx',
+    options: './src/ts/options.ts',
+  },
+  output: {
+    path: `${__dirname}/public/js`,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: ['>= 0.2% and Chrome >= 1'],
+                    useBuiltIns: 'usage',
+                    corejs: 3,
+                  },
+                ],
+              ],
+            },
+          },
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.pcss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-nested'],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: (chunk) => chunk.name !== 'background',
+    },
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+  // // Chrome 拡張においてファイルサイズは大した問題ではないので無視する
+  // performance: {
+  //   hints: false,
+  // },
+};
+
+if (config.mode === 'development') {
+  config = merge(config, {
+    devtool: 'inline-source-map',
+  });
+} else {
+  config = merge(config, {
+    plugins: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            pure_funcs: ['console.log', 'console.info'],
+          },
+        },
+      }),
+      // new BundleAnalyzerPlugin(),
+    ],
+  });
+}
+
+module.exports = config;
