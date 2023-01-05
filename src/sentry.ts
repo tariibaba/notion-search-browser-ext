d0f4cbc7f6e44fad8076f70a358c97c11a4bb278
Cside <cside.story@gmail.com>
2023-01-05 10:58:45 +0900

    Feature: Sentry いったん off

--- src/sentry.ts
+++ src/sentry.ts
@@ -2,15 +2,18 @@ import * as Sentry from '@sentry/browser';
 import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 import { BrowserTracing } from '@sentry/tracing';
 
+const ENABLED = false;
+
 export const initSentry = () => {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    release: VERSION,
-    integrations: [
-      new BrowserTracing(),
-      new CaptureConsoleIntegration({
-        levels: ['warn', 'error'],
-      }),
-    ],
-  });
+  if (ENABLED)
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      release: VERSION,
+      integrations: [
+        new BrowserTracing(),
+        new CaptureConsoleIntegration({
+          levels: ['warn', 'error'],
+        }),
+      ],
+    });
 };
