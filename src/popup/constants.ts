8fb3848234fd5f757cc3d45643fff45eefd0a9bf
Cside <cside.story@gmail.com>
2023-01-13 23:46:33 +0900

    add block types

--- src/popup/constants.ts
+++ src/popup/constants.ts
@@ -30,19 +30,28 @@ export const SORT_BY = {
 //  - 下の CAN_BE_DIR
 //  - apiResponse.d.ts
 export const TABLE_TYPE = {
-  WORKSPACE: 'space',
+  WORKSPACE: 'space', // parent only
   BLOCK: 'block',
-  COLLECTION: 'collection', // parent のみ
-  TEAM: 'team',
+  COLLECTION: 'collection', // parent only
+  TEAM: 'team', // parent only
 } as const;
 
-// NOTE: ここに追加したら、テストも追加。あと popup.pcss のクラスにも一応追加
+// NOTE: ここに追加したら、テストも追加
 export const BLOCK_TYPE = {
   PAGE: 'page',
   COLLECTION_VIEW_PAGE: 'collection_view_page',
   COLLECTION_VIEW: 'collection_view',
   COLUMN_LIST: 'column_list',
   COLUMN: 'column',
+  TEXT: 'text',
+  TO_DO: 'to_do',
+  SUB_HEADER: 'sub_header',
+  TOGGLE: 'toggle',
+  BULLETED_LIST: 'bulleted_list',
+  // Also known as a "Template Button". The title is the button text, and the children are the templates to clone.
+  // https://github.com/jamalex/notion-py/blob/master/notion/block.py#L504
+  // Official: https://www.notion.so/help/template-buttons
+  FACTORY: 'factory',
 } as const;
 
 // default is true
@@ -55,6 +64,13 @@ export const CAN_BE_DIR = {
     [BLOCK_TYPE.COLLECTION_VIEW]: true,
     [BLOCK_TYPE.COLUMN_LIST]: false,
     [BLOCK_TYPE.COLUMN]: false,
+    // 下記は dir にならなくていい。親が必ず block だし。というか子を持てないだろう。
+    [BLOCK_TYPE.TEXT]: false,
+    [BLOCK_TYPE.TO_DO]: false,
+    [BLOCK_TYPE.SUB_HEADER]: false,
+    [BLOCK_TYPE.TOGGLE]: false,
+    [BLOCK_TYPE.BULLETED_LIST]: false,
+    [BLOCK_TYPE.FACTORY]: false, // これ何だろう...。
   },
 } as const satisfies {
   COLLECTION: boolean;
